625d6dca49025afab059dd2e2cc2dc9db1afc80a
Lucas Hyan <lucas.hyan@hotmail.com>
2024-04-01 19:42:30 -0300

    Refactor ContentCard component and add onClick event handler to ContentCards

--- src/layout/ContentCards.jsx
+++ src/layout/ContentCards.jsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import AboutText from "../components/AboutText";
 import ContentCard from "../components/ContentCard";
-export function ContentCards() {
+
+export function ContentCards({ onClick }) {
   return (
     <div className="w-full max-w-screen-lg mx-auto">
       <AboutText />
@@ -9,37 +11,37 @@ export function ContentCards() {
           image="https://source.unsplash.com/random/800x600"
           title="Cartão A"
           paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          link="#"
+          onClick={onClick}
         />
         <ContentCard
           image="https://source.unsplash.com/random/800x601"
           title="Cartão B"
           paragraph="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-          link="#"
+          onClick={onClick}
         />
         <ContentCard
           image="https://source.unsplash.com/random/800x602"
           title="Cartão C"
           paragraph="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          link="#"
+          onClick={onClick}
         />
         <ContentCard
           image="https://source.unsplash.com/random/800x604"
           title="Cartão D"
           paragraph="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-          link="#"
+          onClick={onClick}
         />
         <ContentCard
           image="https://source.unsplash.com/random/800x604"
           title="Cartão E"
           paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          link="#"
+          onClick={onClick}
         />
         <ContentCard
           image="https://source.unsplash.com/random/800x605"
           title="Cartão F"
           paragraph="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-          link="#"
+          onClick={onClick}
         />
 
         <div className="h-20"></div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "../App.css";
 import { AppBar } from "../layout/AppBar/AppBar";
 import { Banner } from "../layout/Banner";
@@ -6,13 +7,20 @@ import { ContentCards } from "../layout/ContentCards";
 import { SiteDescription } from "../layout/SiteDescription";
 import { Footer } from "../layout/Footer";
 import { Form } from "../layout/Form";
+import { ForumPost } from "../layout/ForumPost";
 
 export function HomePage() {
+  const [showCards, setShowCards] = useState(true);
+
+  const handleClick = () => {
+    setShowCards(false);
+  }
+
   return (
     <>
       <AppBar />
       <Banner />
-      <ContentCards />
+      {showCards ? <ContentCards onClick={handleClick} /> : <ForumPost />}
       <Footer />
     </>
   );
